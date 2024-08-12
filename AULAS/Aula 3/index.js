65130c495cc828c4a4db8061f8c56c65568828b8
LukasLuut <lukasluut@gmail.com>
2024-08-12 17:24:56 -0300

    aula 3

---
+++ AULAS/Aula 3/index.js
@@ -0,0 +1,61 @@
+// texto exemplo
+/* comentar mais de uma linha
+linha 1
+linha 2
+linha 3 fim do comentário */
+
+
+console.log("Olá Mundo!")
+console.log("Tudo bem?")
+
+prompt("Qual o seu nome?")
+
+const nome = "Maria"
+const idade = 23
+
+console.log("Olá", "Meu nome é:", nome, "e eu tenho: ", idade, "anos");
+
+const nome1 = "Ingrid"
+const sobrenome = "Bernardes"
+const idade1 = 29
+
+console.log("Hey", "Tudo bem?", "Meu nome e sobrenome é:", nome1, sobrenome, "e eu tenho: ", idade1, "anos");
+
+const estudante = "estudante"
+
+console.log("Eu sou", estudante)
+
+const got = "Game of Thrones"
+const temporadasDeGot = "8";
+
+console.log(typeof got)
+console.log(typeof temporadasDeGot)
+
+let novaVariavel
+console.log(typeof novaVariavel)
+
+novaVariavel = 2
+console.log(typeof novaVariavel)
+
+novaVariavel = undefined
+console.log(typeof novaVariavel)
+
+
+
+const nome2 = prompt ("Qual o seu nome?")
+const idade2 = prompt ("Qual a sua idade?")
+console.log(nome2);
+console.log(idade2);
+
+const idadeNumero = 23
+const idadeTexto = idadeNumero.toString()
+console.log(typeof idadeNumero)
+console.log(typeof idadeTexto)
+
+const idadeTexto2 = "23"
+const idadeNumero2 = Number(idadeTexto2)
+console.log(typeof idadeTexto2)
+console.log(typeof idadeNumero2)
+
+
+
